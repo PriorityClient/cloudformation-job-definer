514da1b32616054d61a7776a0a7a975c80f358d3
Evan Short <evan.short@gmail.com>
2019-01-04 14:50:41 -0500

    building for new version of node

--- build/index.js
+++ build/index.js
@@ -93,7 +93,7 @@ var lambda_for = function lambda_for(job) {
 		"Description": describe_lambda(job),
 		"Handler": "index.handler",
 		"Role": { "Fn::GetAtt": ["JobRunnerRole", "Arn"] },
-		"Runtime": "nodejs4.3",
+		"Runtime": "nodejs8.10",
 		"Timeout": 30,
 		"Environment": { "Variables": {
 				"BUCKET": job.get('tokenBucketName'),
